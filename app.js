4ac29a09bddadf6bf1bafe4abd5a76a5e35d1063
DONGHYEOK95 <dhk951114@gmail.com>
2018-11-03 22:17:48 +0900

    이미지 및, 초기 구동 설정.

--- app.js
+++ app.js
@@ -1,29 +1,79 @@
-var express=require('express');
-var app=express();
-var bodyparser=require('body-parser');
+var express=require('express'); //express=nodejs서버를 구동시키기위한 명령
+var app=express(); //app을 통해 express서버를 구동 app=서버
+var fs = require('fs');
+var bodyparser=require('body-parser'); //스트링을 데이터로 파싱
+var markets = require('./market.js')();
 
-app.use(bodyparser.json());
-app.listen(8080, function(){
+app.use('/images', express.static('images'));
+app.use(bodyparser.json()); // 바디파서로 파싱해서 쓰겟다, 스트링데이터를 쓰겟다
+
+app.listen(8080, function(){ // node app.js 8080포트(임시)를 통해 서버 통신하겠다.
   console.log('server is running');
+  console.log(markets[0]);
 });
 
-app.get('/keyboard', function(req, res) {
+app.get('/keyboard', function(req, res) { //데이터를 받는 양식 http메소드
   var keyboard={
-    "type":"text"
+    "type": "buttons",
+    "buttons": [
+      "1. 매장선택",
+      "2. 메뉴선택",
+      "3, 주문하기",
+      "4. 주문확인"
+    ]
   };
 
   res.send(keyboard);
 });
+
+function jsonToString() {
+  var result = '';
+  var i = 0;
+
+  for (i=0;i<markets.length;i++) {
+    result += (i+1) + '. ' + markets[i].market_name + '(' + markets[i].market_tel + ')\n';
+  }
+
+  return result;
+}
 
 app.post('/message', function(req, res) {
   var user_key = decodeURIComponent(req.body.user_key);
   var type = decodeURIComponent(req.body.type);
   var content = decodeURIComponent(req.body.content);
 
-  var answer = {
-    "message" : {
-      "text" : content
-    }
+  var answer = {};
+
+  if (content == "1. 매장선택") {
+    answer = {
+      "message" : {
+        "photo": "http://54.180.82.68:8080/images/img_1.jpg",
+        "text": "[매장을 선택해 주세요]\n"+jsonToString(),
+        "keyboard": {
+          "type": "text"
+        }
+      }
+    };
+  } else if (content == "2. 메뉴선택") {
+
+  } else if (content == "3. 주문하기") {
+
+  } else if (content == "4. 주문확인") {
+
+  } else {
+    answer = {
+      "message" : {
+        "keyboard": {
+          "type": "buttons",
+          "buttons": [
+            "1. 매장선택",
+            "2. 메뉴선택",
+            "3, 주문하기",
+            "4. 주문확인"
+          ]
+        }
+      }
+    };
   }
 
   res.send(answer);
---
+++ market.js
@@ -0,0 +1,41 @@
+module.exports = function() {
+  var market = [
+    {
+      "market_name": "치킨나라 피자공주",
+      "market_tel": "010-3192-8053"
+      //another properties...
+    },
+    {
+      "market_name": "마켓_2",
+      "market_tel": "010-2222-2222"
+      //another properties...
+    },
+    {
+      "market_name": "마켓_3",
+      "market_tel": "010-3333-3333"
+      //another properties...
+    },
+    {
+      "market_name": "마켓_4",
+      "market_tel": "010-4444-4444"
+      //another properties...
+    },
+    {
+      "market_name": "마켓_5",
+      "market_tel": "010-5555-5555"
+      //another properties...
+    },
+    {
+      "market_name": "마켓_6",
+      "market_tel": "010-6666-6666"
+      //another properties...
+    },
+    {
+      "market_name": "마켓_7",
+      "market_tel": "010-7777-7777"
+      //another properties...
+    },
+    //another markets...
+  ];
+  return market;
+}
