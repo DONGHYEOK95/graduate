37eb50eff15fb59fcd9104f27de3c8dd5f3fe774
DONGHYEOK95 <dhk951114@gmail.com>
2019-03-17 14:03:48 +0900

    no message

--- app3.js
+++ app3.js
@@ -3,20 +3,20 @@ var mecab = require('mecab-ya'); //형태소 분석기 라이브러리.
 
 mecab.pos('후라이드 치킨', function(err,result) {
   console.log(result);
-};
+});
 
 mecab.pos('콜라 추가해줘', function(err,result) {
   console.log(result);
-};
+});
 
 mecab.pos('젓가락 많이줘', function(err,result) {
   console.log(result);
-};
+});
 
 mecab.pos('맥주 2개 추가', function(err,result) {
   console.log(result);
-};
+});
 
 mecab.pos('맥주 1개 빼줘', function(err,result) {
   console.log(result);
-};
+});
