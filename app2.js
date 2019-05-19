e564d76b8447be185de07a16aa6628bdcaab29c6
daesoo94 <tlaeotn123@gmail.com>
2019-05-19 09:46:07 +0900

    no message

--- app2.js
+++ app2.js
@@ -258,7 +258,7 @@ app.post('/message', function(req, res) {
       user[user_key].pay = resultContent[0]?resultContent:'카드';
       user[user_key].address = resultContent[1]?resultContent[1]:'전화 바랍니다';
       // 디비에 저장한다.
-      var orderMenu = getStringMenu(user_key).replace(/(?:\r\n|\r|\n)/g, '<br>');
+      var orderMenu = getStringMenu(user_key).replace(/(?:\r\n|\r|\n)/g, 'br');
       connection.query('INSERT INTO order(`user_key`, `order`) VALUES (\''+user_key+'\', \''+orderMenu + '\')');
 
       delete user[user_key];
